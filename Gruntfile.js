ae1ce17313362ac6b8a18a437ae739138e5b2273
Carlos Araya <carlos.araya@gmail.com>
2015-08-27 12:08:18 -0700

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -214,11 +214,12 @@
       'gh-pages': {
         options: {
           message: 'Content committed from Grunt gh-pages',
-          dotfiles: false,
+          base: 'dist',
+          dotfiles: true,
         },
         // These files will get pushed to the `
         // gh-pages` branch (the default)
-        src: ['dist/**/*']
+        src: ['**/*']
       },
 
       // FILE MANAGEMENT
